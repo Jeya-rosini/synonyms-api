82b4bc90a821bef1d71f7cfe1bd4a1f4b36559ed
Jeya-rosini <jeya.rosini@gmail.com>
2024-01-26 16:21:18 +0530

    feat:console the meaning of the word

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
 //https://wordsapiv1.p.mashape.com/words/${word}/synonyms
+
+const meaning = document.querySelector("#meaning");
+const search = document.querySelector("#search");
+const button = document.querySelector("#button");
+const url = `https://api.dictionaryapi.dev/api/v2/entries/en/`
+
+button.addEventListener('click', () => {
+window.location.href = "/"
+})
+
+
+async function getWord() {
+try {
+    const getUrl = await fetch(url + `${search.value}`);
+    // console.log(getUrl);
+    const jsonUrl = await getUrl.json();
+    // console.log(jsonUrl);
+    return jsonUrl
+} catch (error) {
+    alert("Please enter the write word")
+}
+}
+ 
+const word = await getWord();
+console.log(word[0].meanings[1].synonyms);
